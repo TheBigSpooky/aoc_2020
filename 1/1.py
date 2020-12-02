92e94934eb1be9989f650bd8b68c825e69c75db8
Chris <chris@tgsmc.com>
2020-12-01 21:25:19 -0800

    woo hoo

---
+++ 1/1.py
@@ -0,0 +1,23 @@
+vals = []
+for line in open('inputs'):
+    vals.append(int(line.strip()))
+
+
+def part_1():
+    for i in vals:
+        if 2020 - i in vals:
+            print(f"part1: {i * (2020 - i)}")
+            break
+
+
+def part_2():
+    for i in vals:
+        for j in vals:
+            z = 2020 - i - j
+            if z in vals:
+                print(f"part2: {i * j * z}")
+                return
+
+
+part_1()
+part_2()
